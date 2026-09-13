634fda97d755f3a2a393752eaab4096b4ed392ee
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-09-13 23:05:25 +0600

    implement better auth

--- src/app/modules/specialty/specialty.route.ts
+++ src/app/modules/specialty/specialty.route.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
 import { SpecialtyController } from "./specialty.controller";
-
+import validateRequest from "../../middlewares/validateRequest";
+import { SpecialtyValidation } from "./specialty.validation";
 
 const router = Router();
 
-router.post('/', SpecialtyController.createSpecialty);
-router.get('/', SpecialtyController.getAllSpecialties);
-router.patch('/:id', SpecialtyController.updateSpecialty);
-router.delete('/:id', SpecialtyController.deleteSpecialty);
+router.post(
+  '/',
+  validateRequest(SpecialtyValidation.createSpecialty),
+  SpecialtyController.createSpecialty
+);
 
+router.get('/', SpecialtyController.getAllSpecialties);
+
+router.patch(
+  '/:id',
+  validateRequest(SpecialtyValidation.updateSpecialty),
+  SpecialtyController.updateSpecialty
+);
+
+router.delete('/:id', SpecialtyController.deleteSpecialty);
 
 export const SpecialtyRoutes = router;
